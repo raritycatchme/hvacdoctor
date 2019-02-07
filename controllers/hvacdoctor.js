8af74f27a7b30c658b9ef5ade37db7c50ad60c81
Anthematics <Jasonharder2@gmail.com>
2019-02-06 20:06:50 -0500

    file upload branch almost complete - files upload , save to disk but need proper naming conventions + add file verification (mimetype must be csv)

--- controllers/hvacdoctor.js
+++ controllers/hvacdoctor.js
@@ -5,11 +5,14 @@ var Excel = require('exceljs');
 var workbook1 = new Excel.Workbook();
 var sheet = workbook1.addWorksheet('My Sheet');
 var excel = require("exceljs");
+const index = require("../routes/index")
 
 console.log('hvac doctor loads')
 
 var workbook = new Excel.Workbook();
-workbook.csv.readFile('HVACresultfile')
+workbook.csv.read('HVACresultfile.csv')
     .then(function(worksheet) {
-       console.log('reads')
+        console.log('reads')
     });
+
+
--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HVAC Doctor' });
 });
 
-var upload = multer({ storage: storage })
+var upload = multer({ dest: 'uploads' })
+
+var storage = multer.diskStorage({
+  destination: function (req,file,cb) {
+    cb(null,'uploads')
+  },
+  filename: function (req,file,cb) {
+    cb(null,file.fieldname)
+  }
+})
 
 
-router.post('/upload', upload.single('HVACresultfile'),(req,res,next) => {
+router.post('/upload', upload.single('HVACresultfile.csv'),(req,res,next) => {
   const file = req.file
+
   if (!file) {
     const error = new Error('Please upload a file')
     error.httpStatusCode = 400
     return next (error)
+  } else if (! req.file.path) {
+    const error = new Error('file uploaded no path')
+    error.httpStatusCode = 400
+    return next (error)
   }
-  console.log(file.fieldname);
-  res.send(file)
-})
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
+  console.log(file.name)
+  res.send(file)
 })
 
 module.exports = router;
