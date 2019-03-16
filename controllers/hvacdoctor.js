6bc00aefaea32c5e9aafc11fa28e9cb7a4a6eb5e
Anthematics <Jasonharder2@gmail.com>
2019-03-16 08:18:31 -0400

    code sort of works , need async methods

--- controllers/hvacdoctor.js
+++ controllers/hvacdoctor.js
@@ -5,56 +5,56 @@ var Excel = require('exceljs');
 const index = require("../routes/index")
 const path = require('path')
 const fs = require('fs')
-console.log('hvac doctor loads')
-
 
 var workbook = new Excel.Workbook();
+
 //var myworkbook = workbook.csv.readFile('uploads/HVACresultfile.csv')
 // if no workbook await the file function should go here or something.
 
-function findFile() {
-
-var myworkbook = workbook.csv.readFile('uploads/HVACresultfile.csv')
-var worksheet = workbook.getWorksheet('hvac_report_csv2')
-
-assessFile(myworkbook,worksheet)
-}
-
-function assessFile(myworkbook,worksheet) {
-   myworkbook.then(function(result){
-    try {
-        worksheet.getCell('A6').fill = {
-            type: solid,
-            bgcolor: {argb:'FFFF0000'}
-        }
-    } catch(err){
-        console.log(err)
-    }
-    workbook.xlsx.writeFile('uploads/derf.xlsx')
-})
-
-    writeFile(myworkbook,worksheet)
-}
-
-function formatWorkbook(myworkbook) {
-
-}
-
-
-function writeFile (myworkbook,worksheet) {
-    myworkbook.then(function(result){
-        try {
-            workbook.xlsx.writeFile('uploads/wutwutpromise')
-            console.log('Look in uploads/wutwutpromise')
-        } catch(err){
-            console.log(err)
-        }}
-    )
+function findFile(workbook) {
+    /*function will be to wait for file upload and confirm it is there before
+    calling convert file **move csvworkbook variable when I do adjust.*/
+convertFile(workbook)
 }
 
 findFile()
 
 
+function convertFile(workbook) {
+    var workbook = new Excel.Workbook();
+    csvWorkbook = workbook.csv.readFile('uploads/HVACresultfile.csv')
+    csvWorkbook.then(function(result){
+    try {
+    workbook.xlsx.writeFile('uploads/success.xlsx')
+    console.log('converted')
 
+    } catch(err){
+    console.log(err)
+    }
+})
+
+}
+
+function formatWorkbook(workbook) {
+    var workbook = new Excel.Workbook();
+    var xlsxWorkbook = workbook.xlsx.readFile('uploads/success.xlsx')
+    var xlsxWorksheet = workbook.getWorksheet('sheet1')
+
+    xlsxWorkbook.then(function(result){
+        try {
+            assessFile(xlsxWorkbook,xlsxWorksheet)
+            console.log('assessing')
+        } catch(err) {
+            console.log(err)
+        }
+    })
+}
+
+function assessFile(workbook,xlsxWorkbook,xlsxWorksheet) {
+    console.log('burf')
+}
+
+
+formatWorkbook()
 
 // remove and change readfile() if code does not work.
